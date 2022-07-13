1f4462f2c97ceea1c6a2d9261575ca48056d2297
ChrisWang <chrisqq13@gmail.com>
2022-07-13 16:22:07 +0800

    Replace allocator.h for construct.h

---
+++ allocator/allocator.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <new.h>
+
+namespace MySTL {
+    // allocator
+    template <class _T>
+    struct allocator {
+    public:
+        typedef _T          value_type;
+        typedef size_t      size_type;
+        typedef ptrdiff_t   difference_type;
+        // constructor, no visble effect.
+        constexpr allocator() noexcept;
+        constexpr allocator(const allocator& other ) noexcept;
+        template< class U >
+        constexpr allocator( const allocator<U>& other ) noexcept;
+        // destructor destroys the default allocator.
+        constexpr ~allocator();
+        // allocate storage, construct object in allocator_traits. 
+        constexpr _T* allocate(size_type n);
+        // deallocate storage, destroy object in allocator_traits. 
+        constexpr void deallocate(_T* p, size_type n);
+    };
+
+    template<class _T>
+    constexpr _T* allocator<_T>::allocate(size_type n) {
+        if(n < 0) {
+            throw std::bad_alloc;
+        }
+        else if (n == 0) {
+            return nullptr;
+        }
+        else {
+            return static_cast<_T*>(::operator new(n * sizeof(_T)));
+        }
+    }
+    template<class _T>
+    constexpr void allocator<_T>::deallocate(_T* p, size_type n) {
+        if (p == nullptr)
+            return;
+        ::operator delete(p);
+    }
+}
